ba26cd156dd35ed2437ecd2429b1e1a374261a32
JasperWoortman <jasper.woortman@hva.nl>
2020-10-03 11:55:27 +0200

    Final version

--- app/src/main/java/com/example/madlevel1task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel1task2/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.madlevel1task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.madlevel1task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnSubmit.setOnClickListener {
+            checkAnswer()
+        }
+    }
+    private fun checkAnswer() {
+        var correctAnswers = 0
+
+        val answer1 = binding.etAnswer1.text.toString()
+        val answer2 = binding.etAnswer2.text.toString()
+        val answer3 = binding.etAnswer3.text.toString()
+        val answer4 = binding.etAnswer4.text.toString()
+
+        if (answer1 == getString(R.string.tvT)) {
+            correctAnswers += 1
+        }
+
+        if (answer2 == getString(R.string.tvF)) {
+            correctAnswers += 1
+        }
+
+        if (answer3 == getString(R.string.tvF)) {
+            correctAnswers += 1
+        }
+
+        if (answer4 == getString(R.string.tvT)) {
+            correctAnswers += 1
+        }
+
+        Toast.makeText(this, "You have $correctAnswers Correct Answers",Toast.LENGTH_LONG).show()
+
     }
 }
